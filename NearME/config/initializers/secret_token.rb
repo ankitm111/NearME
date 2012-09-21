2cff5a18a539539de21c0e5e9cff71ca520458e6
ankitm111 <ankitm111@gmail.com>
2012-09-21 17:42:15 -0400

    a new rails application altogether

---
+++ NearME/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NearME::Application.config.secret_token = '178f3df529746ad9320ea13e36ae29adcc5e9830ebc93f3df010b1da4e000843d4b854fed49db3f67f961c0b92c09ad2d9cdd6ad67520f425d7bd0155503c330'
